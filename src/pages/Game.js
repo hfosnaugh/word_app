b63c951f235dfacbf61dc51801087f1ccfed62f5
hfosnaugh <hannah.l.fosnaugh@gmail.com>
2025-08-13 13:22:48 -0400

    centered elements. working on UI design

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,5 +1,5 @@
 const Game = () => {
-  return <h1 class='display-2'>Game</h1>;
+  return <h1 style={{ textAlign: "center" }} className='display-2'>Game</h1>;
 };
 
 export default Game;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <>
-      <h1 class='display-2'>Home</h1>
+      <h1 style={{ textAlign: "center" }} className='display-2'>Home</h1>
+      <div class="wrapper">
       <Link to="/game">
         <Button variant="primary" className='Button' size="lg">
           Go to Game
         </Button>
       </Link>
+      </div>
     </>
   );
 }
