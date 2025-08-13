9f08cb2d2a705901949794e7ce5f5d125e3bb721
Hannah Fosnaugh <hannah.l.fosnaugh@gmail.com>
2025-08-12 21:01:28 -0400

    Feature layout (#1)
    * working on layout/components
    
    * added favicon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-//import React from 'react'; maybe pointless
 import { Routes, Route } from 'react-router-dom';
-import Layout from './pages/Layout.js';
+import Navbar from './pages/Navbar.js';
 import HomePage from './pages/HomePage.js';
 import Game from './pages/Game.js';
 import ERR404 from './pages/ERR404.js';
@@ -9,8 +8,8 @@ import './styles.css';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/home" index element={<HomePage />} />
+      <Route path="/" element={<Navbar />}>
+        <Route path="/" index element={<HomePage />} />
         <Route path="/game" element={<Game />} />
         <Route path="*" element={<ERR404 />} />
       </Route>
--- src/pages/ERR404.js
+++ src/pages/ERR404.js
@@ -1,5 +1,5 @@
 const ERR404 = () => {
-  return <h1>Error 404</h1>;
+  return <h1 class='display-2'>Error 404</h1>;
 };
 
 export default ERR404;
