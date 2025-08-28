43b469b22bc3f048349678c8ce5d7a0c11a7d12c
Hannah Fosnaugh <hannah.l.fosnaugh@gmail.com>
2025-08-28 14:11:36 -0400

    centered elements on home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <>
-      <h1 class='display-2'>Home</h1>
+    <div class="container" style={{height: '40vh'}}><h1 class='display-2'>Home</h1></div>
+    <div class="container">
       <Link to="/game">
         <Button variant="primary" className='Button' size="lg">
           Go to Game
         </Button>
       </Link>
+      </div>
     </>
   );
 }
