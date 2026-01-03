245db97104d75598c709656c7a22db85c9e37bec
hfosnaugh <hannah.l.fosnaugh@gmail.com>
2026-01-03 00:13:15 -0500

    messing around with textbox

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,38 @@
 const Game = () => {
-  return <h1 style={{ textAlign: "center" }} className='display-2'>Game</h1>;
+  //return <h1 style={{ textAlign: "center" }} className='display-2'>Game</h1>;
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }} className='display-2'>Game</h1>
+    <form method="post" onSubmit={handleSubmit}>
+        <label>
+          Enter a new word that can 'fit' into the word:
+          <textarea
+            name="newWord"
+            rows={4}
+            cols={40}
+          />
+        </label>
+        <hr />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default Game;
 //defines single JSX component for each page and returns it as a simple element
+
+//working on game formatting today
